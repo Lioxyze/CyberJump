828778f7173772f616f0071baf9d1e3ef487a8f3
Lioxyze <lorenzopro38@gmail.com>
2024-03-15 13:36:16 +0100

    Update

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,34 +1,38 @@
-let container = document.querySelector(".container");
+let container = document.querySelector("body");
 let image = document.createElement("img");
 image.src = "assets/img/Personnage.PNG";
 image.style.height = "100px";
 image.style.position = "absolute";
-image.style.bottom = "75px";
+image.style.right = "590px";
+image.style.bottom = "70px";
 image.classList.add("mario");
 container.appendChild(image);
 let mario = document.querySelector(".mario");
 
-let positionXMario = 30;
+let positionXMario = 0;
 
 document.body.onkeyup = function (e) {
   if (e.key == " " || e.code == "Space" || e.keyCode == 32) {
-    mario.style.bottom = "400px";
+    mario.style.bottom = "200px";
+    mario.style.transition = "1s";
     mario.classList.add("transition");
     setTimeout(() => {
-      mario.style.bottom = "75px";
+      mario.style.bottom = "70px";
     }, 999);
-  }
-
-  if (e.keyCode == "37") {
-    positionXMario -= 30;
-    mario.style.left = `${positionXMario}px`;
-    mario.style.transition = "1s";
-  } else if (e.keyCode == "39") {
-    positionXMario += 30;
-    mario.style.left = `${positionXMario}px  `;
     mario.style.transition = "1s";
   }
 };
+
+//   if (e.keyCode == "37") {
+//     positionXMario -= 30;
+//     mario.style.left = `${positionXMario}px`;
+//     mario.style.transition = "1s";
+//   } else if (e.keyCode == "39") {
+//     positionXMario += 30;
+//     mario.style.left = `${positionXMario}px  `;
+//     mario.style.transition = "1s";
+//   }
+// };
 
 // let container = document.querySelector(".container");
 // let image = document.createElement("img");
