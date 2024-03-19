cab676e7f6c2ec2a7d010e5e66dfbba21f16d287
Lioxyze <lorenzopro38@gmail.com>
2024-03-19 14:27:49 +0100

    update

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,63 +1,65 @@
-// class Personnage {
-//   constructor(jump, life, Pseudo) {
-//     this.jump = jump;
-//     this.life = life;
-//     this.life = Pseudo;
-//   }
-//   spawnPersonnage() {
-//     let container = document.querySelector("body");
-//     let Perso1 = document.createElement("img");
-//     Perso1.src = "assets/img/Personnage.PNG";
-//     Perso1.style.height = "100px";
-//     Perso1.style.position = "absolute";
-//     Perso1.style.right = "590px";
-//     Perso1.style.bottom = "70px";
-//     Perso1.classList.add("david");
-//     container.appendChild(Perso1);
-//   }
+// Sélectionne le conteneur
+let container = document.querySelector(".container");
 
-//   sauter() {
-//     let david = document.querySelector(".david");
-//     document.body.onkeyup = function (e) {
-//       if (e.key == " " || e.code == "Space" || e.keyCode == 32) {
-//         david.style.bottom = "200px";
-//         david.style.transition = "1s";
-//         david.classList.add("transition");
-//         setTimeout(() => {
-//           david.style.bottom = "70px";
-//         }, 999);
-//         david.style.transition = "1s";
-//       }
-//     };
-//   }
-// }
-
-let container = document.querySelector("body");
+// Crée les images pour les personnages
 let Perso1 = document.createElement("img");
 let Perso2 = document.createElement("img");
+let mechant = document.createElement("img");
+
+// Intègre l'image
 Perso1.src = "assets/img/Personnage.PNG";
 Perso2.src = "assets/img/Lucy.png";
+mechant.src = "assets/img/Mob.gif";
+
+// Règle la taille des personnages
 Perso1.style.height = "100px";
 Perso2.style.height = "100px";
+mechant.style.height = "50px";
+
+// Règle la position des personnages
 Perso1.style.position = "absolute";
 Perso2.style.position = "absolute";
-Perso1.style.right = "750px";
-Perso2.style.right = "450px";
+mechant.style.position = "absolute";
+
+// Règle la position des personnages
+Perso1.style.right = "450px";
+Perso2.style.right = "250px";
+mechant.style.right = "200px";
+
+// Règle la position des personnages
 Perso1.style.bottom = "180px";
 Perso2.style.bottom = "175px";
+mechant.style.bottom = "175px";
+
+// Ajoute une classe pour chaque personnage
 Perso1.classList.add("david");
 Perso2.classList.add("lucy");
+mechant.classList.add("mechant");
+
+// Ajoute les personnages au conteneur
 container.appendChild(Perso1);
 container.appendChild(Perso2);
+container.appendChild(mechant);
 
+// Sélectionne David et Lucy
 let david = document.querySelector(".david");
 let lucy = document.querySelector(".lucy");
 
-let positionXMario = 0;
+// Définit une fonction pour mettre à jour le score
+function updateScore() {
+  score++; // Incrémente le score
+  document.getElementById("score").innerText = score; // Met à jour l'affichage du score
+}
 
+let score = 0; // Initialise le score à 0
+updateScore(); // Met à jour l'affichage initial du score
+setInterval(updateScore, 1000);
+
+// Permet de bloquer l'animation et d'exécuter qu'une fois
 let CantJumpDavid = false;
 let CantJumpLucy = false;
 
+// Permet d'exécuter le saut de David sur la touche ArrowUp
 document.body.onkeyup = function (e) {
   if (
     (e.key == "ArrowUp" || e.code == "ArrowUp" || e.keyCode == 38) &&
@@ -75,6 +77,10 @@ document.body.onkeyup = function (e) {
     }, 999);
   }
 
+  // Met à jour le score
+  updateScore();
+
+  // Permet d'exécuter le saut de Lucy sur la touche Space
   if ((e.key == " " || e.code == "Space" || e.keyCode == 32) && !CantJumpLucy) {
     CantJumpLucy = true;
     lucy.style.bottom = "300px";
@@ -84,10 +90,24 @@ document.body.onkeyup = function (e) {
       lucy.style.bottom = "180px";
       setTimeout(() => {
         CantJumpLucy = false;
-      }, 1000); // Temps de l'animation (1s) + 1ms pour assurer qu'il a fini
+      }, 1000);
     }, 999);
   }
+
+  // let david = { x: 5, y: 5 };
+  // let mechant = { x: 20, y: 10 };
+
+  // if (
+  //   david.x < mechant.x + mechant.width &&
+  //   david.x + david.width > mechant.x &&
+  //   david.y < mechant.y + mechant.height &&
+  //   david.height + david.y > mechant.y
+  // ) {
+  //   // collision détectée !
+  // }
 };
+// const random = document.querySelector(".mechant");
+// random.style.setProperty("--animation-time", time + "s");
 
 //   if (e.keyCode == "37") {
 //     positionXMario -= 30;
@@ -100,34 +120,5 @@ document.body.onkeyup = function (e) {
 //   }
 // };
 
-// let container = document.querySelector(".container");
-// let image = document.createElement("img");
-// image.src = "assets/img/Personnage.PNG";
-// image.style.height = "100px";
-// image.style.position = "absolute";
-// image.style.bottom = "75px";
-// image.classList.add("mario");
-// container.appendChild(image);
-// let mario = document.querySelector(".mario");
-
-// let positionXMario = 30;
-
-// document.body.onkeyup = function (e) {
-//   if (e.key == " " || e.code == "Space" || e.keyCode == 32) {
-//     mario.style.bottom = "400px";
-//     mario.classList.add("transition");
-//     setTimeout(() => {
-//       mario.style.bottom = "75px";
-//     }, 999);
-//   }
-
-//   if (e.keyCode == "37") {
-//     positionXMario -= 30;
-//     mario.style.left = `${positionXMario}px`;
-//     mario.style.transition = "1s";
-//   } else if (e.keyCode == "39") {
-//     positionXMario += 30;
-//     mario.style.left = `${positionXMario}px  `;
-//     mario.style.transition = "1s";
-//   }
-// };
+// const random = document.querySelector(".mechant");
+// random.style.setProperty("--animation-time", time + "s");
