3e84b0560683a8def5801137d97e99b8fec90b92
Lioxyze <lorenzopro38@gmail.com>
2024-03-22 15:36:39 +0100

    update

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,19 +1,4 @@
-var elem = document.querySelector(".container");
-
-function openFullscreen() {
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-  } else if (elem.webkitRequestFullscreen) {
-    /* Safari */
-    elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) {
-    /* IE11 */
-    elem.msRequestFullscreen();
-    domReady(fonction);
-  }
-}
-
-var elem = document.querySelector(".container");
+let elem = document.querySelector(".container");
 
 function openFullscreen() {
   if (elem.requestFullscreen) {
