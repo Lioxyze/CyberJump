b5e964c62482b457cac8db57007fb62ca36731ff
Lioxyze <lorenzopro38@gmail.com>
2024-03-26 09:20:52 +0100

    Update Final

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,23 +1,16 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+////////////////////////
+////////Modal Regle//////
+////////////////////////
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
+let modal = document.getElementById("myModal");
+let btn = document.getElementById("myBtn");
+let span = document.getElementsByClassName("close")[0];
 btn.onclick = function () {
   modal.style.display = "block";
 };
-
-// When the user clicks on <span> (x), close the modal
 span.onclick = function () {
   modal.style.display = "none";
 };
-
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
@@ -42,6 +35,21 @@ ModalValidateBtn.addEventListener("click", () => {
   // Cacher le background de la modal
   ModalBackground.style.display = "none";
   container.style.display = "block";
+
+  let modal = document.getElementById("myModal");
+  let btn = document.getElementById("myBtn");
+  let span = document.getElementsByClassName("close")[0];
+  btn.onclick = function () {
+    modal.style.display = "block";
+  };
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
 
   ////////////////////////
   ////////Score///////////
